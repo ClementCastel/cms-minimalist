d1af7875fea8d972f4169f8773958967bff96dcb
Clément Castel <36570169+Th3Sword290@users.noreply.github.com>
2018-05-03 18:02:39 +0200

    Chapitre 5
    CMS #5

---
+++ article.php
@@ -0,0 +1,29 @@
+<?php
+$id = $_GET['id'];
+ ?>
+
+<html>
+<head>
+  <title>Accueil</title>
+  <meta charset="utf-8" />
+</head>
+<body>
+  <a href="./index.php">Retour à l'index</a><br /><br />
+  <?php
+  if (isset($_GET['id'])){
+  $conn = mysqli_connect('localhost', 'root','', 'cms');
+
+  $sql = "SELECT * FROM articles WHERE ID = '$id'";
+  $result = mysqli_query($conn, $sql);
+
+    while ($data = mysqli_fetch_assoc($result)){
+      echo "TITRE: ".$data['title']."<br /><br />";
+      echo "SOUS-TITRE: ".$data['subtitle']."<br /><br />";
+      echo "CONTENU: ".$data['content']."<br /><br />";
+      echo "DATE: ".$data['date']."<br /><br />";
+      echo "NOTE: ".$data['note']."<br /><br />";
+    }
+  }
+  ?>
+</body>
+</html>
